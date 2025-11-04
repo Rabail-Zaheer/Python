466631389000e41a602e4ae4a6d751c25227e1fd
rabailzaheer <rabailzaheer@gmail.com>
2025-11-04 12:48:07 +0500

    Lesson2

---
+++ Lesson2.py
@@ -0,0 +1,8 @@
+print("Hello Gurneet\n")
+print("Gurneet is a sweet kid\n")
+print("She is in Grade 7\n")
+print("Hello", 5)
+myname = "gurneet"
+age = 12
+grade = 7
+print(f"Hello my name is {myname} and my age is {age}, and i am in grade {grade}")
