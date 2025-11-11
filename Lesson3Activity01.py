4d35e3486214fef558cf8e7fe496707f5d32b1ff
rabailzaheer <rabailzaheer@gmail.com>
2025-11-11 12:10:46 +0500

    Activity 01 for the Lesson 3 done

---
+++ Lesson3Activity01.py
@@ -0,0 +1,28 @@
+name_of_student = "Gurneet"
+
+print("name_of_student has datatype of", type(name_of_student))
+print(f"{name_of_student}")
+
+is_student = True
+print("is_student has a datatype of", type(is_student))
+
+is_a_athelete = False
+print("is_a_athelete has a datatype of", type(is_a_athelete))
+
+student_age = 12
+print("studen_age has a datatype of", type(student_age))
+
+obtained_percentage = 100.0
+print("obtained_percentage has a datatype of", type(obtained_percentage))
+
+
+
+in_city = "brisbane"
+
+age = -100
+
+gurneets_score_on_code = 100000.0
+
+gurneetis1000 = False
+
+print("in_city has a datatype of", type(in_city))
