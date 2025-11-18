b748ff2324339ffac5789f7debae2d7748f73b53
rabailzaheer <rabailzaheer@gmail.com>
2025-11-18 12:18:43 +0500

    Lesson 4 Activity 01 is complete

---
+++ GurneetLesson4Activity01.py
@@ -0,0 +1,19 @@
+print("Calculating Average of the number of leaves on a 5 trees\n")
+
+tree01 = 0
+
+tree02 = 10
+
+tree03 = 200
+
+tree04 = 3000
+
+tree05 = 40000
+
+sum = tree01 + tree02 + tree03 + tree04 + tree05
+
+print(f"The total number of leaves on the trees is {sum}\n")
+
+average = sum/5
+
+print(f"\nThe average number of leaves is {average}")
---
+++ Lesson4Activity01.py
@@ -0,0 +1,18 @@
+print("Let's calculate the average of the leaves of the trees\n")
+
+tree01 = 100
+tree02 = 200
+tree03 = 1000
+tree04 = 300
+tree05 = 400
+tree06 = 500
+tree07 = 600
+tree08 = 700
+tree09 = 1000
+
+sum = tree01 + tree02 + tree03 + tree04 + tree05 + tree06 + tree07 + tree08 + tree09
+print("The sum of all the leaves of the trees are", sum)
+
+Average = sum/9
+
+print(f"\nThe average is {Average}\n")
