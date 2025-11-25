e3a5fdb93b19cd1f505b38ad54382ba09f596975
rabailzaheer <rabailzaheer@gmail.com>
2025-11-25 12:31:11 +0500

    Lesson5 Done

---
+++ Lesson5Activity01.py
@@ -0,0 +1,22 @@
+'''
+Math = 90
+
+if Math == 100:
+    print("Gurneet you got A+")
+    print("Gurneet got 100! marks in Mathematics")
+else:
+    print("You can do better!") 
+
+    '''
+
+# Rupees = 5
+Rupees = 10
+# Rupees = 0
+
+if Rupees == 10:
+    print("I want to buy a chocolate🍫")
+elif Rupees == 5:
+    print("I want to buy a candy🍭")
+else:
+    print("I would like something for free💲")
+
