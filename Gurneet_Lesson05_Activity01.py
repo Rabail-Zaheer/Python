e02e8c8a22531d6e8486d85bb77aba3beba8440b
rabailzaheer <rabailzaheer@gmail.com>
2025-11-25 12:31:27 +0500

    Gurneet Lesson 5 done

---
+++ Gurneet_Lesson05_Activity01.py
@@ -0,0 +1,30 @@
+'''
+
+score = 700
+
+if score == 100:
+
+print("You did okay")
+
+elif score == 500:
+
+print("You can do way better than that")
+
+else:
+
+print("You did amazing, See i told you that you could do better")
+
+'''
+
+#dollars = 500
+
+#dollars = 1000
+
+dollars = 0
+
+if dollars == 1000:
+    print("I would like a Labubu 🧸 because I am rich 💵 💸like that")
+elif dollars == 500:
+    print("I guess I'll just buy a lafufu🦴 because I'm poor like that")
+else:
+    print("I'm broke, umm can u pretty please with a cherry🍒 on top buy for me and if you don't I'll kill you '🔫pew pew🔫'")
